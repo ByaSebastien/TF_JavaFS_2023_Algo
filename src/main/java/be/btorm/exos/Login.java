343ba15b4e11a001192aed1e15280f14875266f3
ByaSebastien <byasebastien@hotmail.com>
2023-10-13 11:13:29 +0200

    login

---
+++ src/main/java/be/btorm/exos/Login.java
@@ -0,0 +1,29 @@
+package be.btorm.exos;
+
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String password;
+        int nbTentative = 0;
+        boolean isBlocked = false;
+
+        do{
+            System.out.print("Password : ");
+            password = scanner.next();
+            nbTentative++;
+            if(nbTentative == 3){
+                isBlocked = true;
+            }
+        }while (!password.equals("Toto") && !isBlocked);
+
+        if(isBlocked){
+            System.out.println("Blocked");
+        }else{
+            System.out.println("Welcome");
+        }
+    }
+}
